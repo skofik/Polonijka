b28d29dc8130792a77f58b167bc74c84b318aceb
Daniel Skowron <skofikit@gmail.com>
2019-06-14 19:53:59 +0200

    correction media-queries and semantic

--- script.js
+++ script.js
@@ -2,7 +2,10 @@
 
 const buttonForm = document.querySelector(".buttonForm");
 const allElementForm = [...document.querySelectorAll(".form input")];
-
+const spanPermision = document.querySelector(".permission span.rules ");
+const statement = document.querySelector(".statement");
+const statementButton = document.querySelector(".statementButton");
+const statementText = document.querySelector(".statement p");
 const form = {
   name: "",
   email: "",
@@ -10,24 +13,30 @@ const form = {
 };
 
 const checkedValue = e => {
-  const { type, value } = e.target;
-  if (type === "text") {
+  const { type, value, className } = e.target;
+  if (className === "name") {
     if (e.target.value !== "") {
+      e.target.style.borderColor = "#939393";
       form.name = true;
     } else {
+      e.target.style.borderColor = "#ff0000";
       form.name = false;
     }
   } else if (type === "email") {
     if (value !== "" && value.includes("@") === true) {
       form.email = true;
+      e.target.style.borderColor = "#939393";
     } else {
       form.email = false;
+      e.target.style.borderColor = "#ff0000";
     }
-  } else if (e.target.className === "rules") {
+  } else if (className === "rules") {
     if (e.target.checked === true) {
       form.checkbox = true;
+      spanPermision.style.color = "#141e46";
     } else {
       form.checkbox = false;
+      spanPermision.style.color = "#ff0000";
     }
   }
 };
@@ -36,11 +45,38 @@ allElementForm.map(item => {
 });
 
 const SendForm = e => {
+  e.preventDefault();
+
+  statement.style.left = "0%";
+  statementButton.addEventListener("click", () => {
+    statement.style.left = "200%";
+  });
   if (form.checkbox === true && form.email === true && form.name === true) {
-    alert("formularz został wysłany");
+    statementText.textContent = "Gratulujemy twój formularz został wysłany";
+    allElementForm.forEach(item => {
+      if (
+        item.type === "email" ||
+        item.type === "text" ||
+        item.type === "tel"
+      ) {
+        item.value = "";
+      } else if (item.type === "checkbox") {
+        item.checked = false;
+      }
+    });
   } else {
-    e.preventDefault();
-    alert("nie wysłano formularza proszę poprawić błędy");
+    statementText.textContent =
+      "Niestety formularz nie został wysłany, należy poprawić błędy zaznaczone na czerwono";
+    allElementForm.forEach(item => {
+      if (
+        (item.className === "email" && form.email === "") ||
+        (item.className === "name" && form.name === "")
+      ) {
+        item.style.borderColor = "#ff0000";
+      } else if (item.className === "rules" && form.checkbox === "") {
+        spanPermision.style.color = "#ff0000";
+      }
+    });
   }
 };
 buttonForm.addEventListener("click", SendForm);
